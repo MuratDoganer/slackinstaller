59bb4369a396de46bff440a52ea6e00c1f1aed3b
Murat Doganer <67741877+MuratDoganer@users.noreply.github.com>
2020-09-02 17:01:28 +0100

    slack installer code

---
+++ SlackInstaller.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Version 0.1 by Murat Doganer, Land Technologies 2020
+# This script installs Slack for deployment via Fleetsmith
+
+# Lets make our directory
+mkdir Slack
+cd Slack
+
+# Lets download the most current slack dmg - must be manually changed
+curl https://downloads.slack-edge.com/releases/macos/4.8.0/prod/x64/Slack-4.8.0-macOS.dmg --output slack.dmg
+
+# Lets mount that dmg now
+hdiutil attach slack.dmg
+
+# And copy the Slack App into the Applications Folder
+cp -R '/Volumes/Slack.app/Slack.app' '/Applications'
+
+# Clean up
+hdiutil detach /Volumes/Slack.app
+rm slack.dmg
+cd /Users/murat/Projects/Scripts 
+rm -r Slack
+exit 0 # on Success
